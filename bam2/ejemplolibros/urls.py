c7b117601b8592e00f77b6bbd9d4f6ae1686dbce
gerson quiroz <gersonquiroz66@gmail.com>
2023-11-26 06:23:12 -0300

    se agrega busqueda

--- bam2/ejemplolibros/urls.py
+++ bam2/ejemplolibros/urls.py
@@ -1,6 +1,6 @@
 # En el archivo urls.py de tu aplicación (miapp)
 from django.urls import path
-from .views import borrar_libro, listar_libros, crear_libro, modificar_libro
+from .views import borrar_libro, listar_libros, crear_libro, modificar_libro, buscar_libros
 
 urlpatterns = [
     # Configura la URL libros/ que mapea a la vista listar_libros
@@ -8,4 +8,5 @@ urlpatterns = [
     path('libros/crear/', crear_libro, name='crear_libro'),
     path('libros/modificar/<int:id>/', modificar_libro, name='modificar_libro'),
     path('libros/borrar/<int:id>/', borrar_libro, name='borrar_libro'),
+    path('libros/buscar/', buscar_libros, name='buscar_libros'),
 ]
--- bam2/ejemplolibros/views.py
+++ bam2/ejemplolibros/views.py
@@ -4,27 +4,35 @@ from .models import Libro
 from .forms import LibroForm
 
 def listar_libros(request):
-    libros = Libro.objects.all()
     form = LibroForm()
-
+    query = request.GET.get('q', '')
+    
     if request.method == 'POST':
         form = LibroForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('listar_libros')
-        else:
-            form = LibroForm()
-    return render(request, 'libros/listar_libros.html', {'libros': libros, 'form': form})
+
+    if query:
+        libros = Libro.objects.filter(titulo__icontains=query)
+    else:
+        libros = Libro.objects.all()
+
+    return render(request, 'libros/listar_libros.html', {'libros': libros, 'form': form, 'query': query})
 
 def crear_libro(request):
+    return _manejar_formulario(request, LibroForm, 'libros/crear_libros.html')
+
+def _manejar_formulario(request, Formulario, template):
+    form = Formulario()
+
     if request.method == 'POST':
-        form = LibroForm(request.POST)
+        form = Formulario(request.POST)
         if form.is_valid():
             form.save()
             return redirect('listar_libros')
-    else:
-        form = LibroForm()
-    return render(request, 'libros/crear_libros.html', {'form': form})
+
+    return render(request, template, {'form': form})
 
 def modificar_libro(request, id):
     libro = get_object_or_404(Libro, id=id)
@@ -47,3 +55,9 @@ def borrar_libro(request, id):
         return redirect('listar_libros')
 
     return render(request, 'libros/borrar_libro.html', {'libro': libro})
+
+def buscar_libros(request):
+    query = request.GET.get('q', '')  # Obtiene el término de búsqueda de la URL
+    libros = Libro.objects.filter(titulo__icontains=query)  # Realiza la búsqueda
+
+    return render(request, 'libros/buscar_libros.html', {'libros': libros, 'query': query})
