706acbecbe27e967e9014683858ad549d5dac087
gerson quiroz <gersonquiroz66@gmail.com>
2023-11-26 05:41:40 -0300

    uso de import

--- bam2/ejemplolibros/views.py
+++ bam2/ejemplolibros/views.py
@@ -4,28 +4,19 @@ from .models import Libro
 from .forms import LibroForm
 
 def listar_libros(request):
-    """
-    Muestra una lista de todos los libros en la base de datos.
-
-    Parameters:
-    - request: HttpRequest
-
-    Returns:
-    - HttpResponse: Renderiza la página 'listar_libros.html' con la lista de libros.
-    """
     libros = Libro.objects.all()
-    return render(request, 'libros/listar_libros.html', {'libros': libros})
+    form = LibroForm()
+
+    if request.method == 'POST':
+        form = LibroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_libros')
+        else:
+            form = LibroForm()
+    return render(request, 'libros/listar_libros.html', {'libros': libros, 'form': form})
 
 def crear_libro(request):
-    """
-    Permite la creación de un nuevo libro mediante un formulario.
-
-    Parameters:
-    - request: HttpRequest
-
-    Returns:
-    - HttpResponse: Renderiza la página 'crear_libros.html' con el formulario de creación de libros.
-    """
     if request.method == 'POST':
         form = LibroForm(request.POST)
         if form.is_valid():
@@ -36,16 +27,6 @@ def crear_libro(request):
     return render(request, 'libros/crear_libros.html', {'form': form})
 
 def modificar_libro(request, id):
-    """
-    Permite la modificación de un libro existente mediante un formulario.
-
-    Parameters:
-    - request: HttpRequest
-    - id: int, ID del libro a modificar.
-
-    Returns:
-    - HttpResponse: Renderiza la página 'modificar_libro.html' con el formulario prellenado del libro a modificar.
-    """
     libro = get_object_or_404(Libro, id=id)
 
     if request.method == 'POST':
@@ -59,16 +40,6 @@ def modificar_libro(request, id):
     return render(request, 'libros/modificar_libro.html', {'form': form})
 
 def borrar_libro(request, id):
-    """
-    Permite la eliminación de un libro existente.
-
-    Parameters:
-    - request: HttpRequest
-    - id: int, ID del libro a eliminar.
-
-    Returns:
-    - HttpResponse: Renderiza la página 'borrar_libro.html' para confirmar la eliminación del libro.
-    """
     libro = get_object_or_404(Libro, id=id)
 
     if request.method == 'POST':
